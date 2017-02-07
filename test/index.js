b348f6a340f3f282fca8c05bfecbe2061e4e6670
theteapot <tin.teapot@gmail.com>
2017-02-08 02:22:54 +1300

    started streamlining out spotify.js

--- test/index.js
+++ test/index.js
@@ -2,9 +2,19 @@
 
 var assert = require('assert');
 var myDiscordBot = require('../lib');
+var spotify = require('../lib/spotify/spotify.js')
+
+describe("hooks", function() {
+	before(function() {
+		spotify.main()
+	})
+})
+
+console.info(myDiscordBot)
 
 describe('myDiscordBot', function () {
   it('should have unit test!', function () {
     assert(false, 'we expected this package author to add actual unit tests.');
   });
 });
+
